462f953ae93f95e2c0dbf6061ce141efd359a59f
Dupopking <dupopking@gmail.com>
2025-01-18 17:00:35 -0500

    added button functions

--- scripts.js
+++ scripts.js
@@ -27,32 +27,49 @@ function operate(operator,num1,num2){
         return multiply(num1,num2)
     }
     else if (operator == "/"){
+        if (num2==0){
+            display.value = "Can't divide by 0"
+            return null
+        }
         return divide(num1,num2)
     }
+    else if (operator == "="{
+        
+    })
 }
 
 function clr() { 
     document.getElementById("result").value = "" 
 } 
 
-function display(val){
-    document.getElementById("result").value += val
-}
 
-const display = document.getElementById('result');
+const display = document.getElementById("result");
 
 const digitButtons = document.querySelectorAll('.digit');
 
 digitButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const currentDisplay = display.textContent;
-  
+      const currentDisplay = display.value;
       // Prevent multiple leading zeros
       if (currentDisplay === '0') {
-        display.textContent = button.value;
+        display.value = button.value;
       } else {
-        display.textContent += button.value;
+        display.value += button.value;
       }
     });
   });
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const currentDisplay = display.value;
+      // Prevent multiple leading zeros
+      if (currentDisplay === '0') {
+        display.value = button.value;
+      } else {
+        display.value += button.value;
+      }
+    });
+  });
+
 
